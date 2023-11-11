8eeaa6393919c5dabb8f46ea2d72ad918f45e5d1
Joseph Bak <joseph.bak31415@gmail.com>
2023-11-10 23:06:42 -0500

    mario file using loop

--- lecture1-c/mario.c
+++ lecture1-c/mario.c
@@ -1,21 +1,39 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int get_size(void);
+void print_grid(int size);
+
 int main(void)
 {
-    // const int n = get_int("Size: ");
+    // Get size of grid
+    int n = get_size();
+    
+    // Print grid of bricks
+    print_grid(n);
+}
+
+int get_size(void)
+{
     int n;
     do
     {
         n = get_int("Size: ");
-    } while (n < 1);
-    
-    for (int i = 0; i < n; i++)
+    }
+    while (n < 1);
+    return n;
+
+}
+
+void print_grid(int size)
+{
+    for (int i = 0; i < size; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < size; j++)
         {
             printf("#");
         }
         printf("\n");
     }
+
 }
