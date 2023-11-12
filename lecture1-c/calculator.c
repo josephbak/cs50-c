fa1623de45517f35e80158af458d9fa2721d0d84
Joseph Bak <joseph.bak31415@gmail.com>
2023-11-12 13:31:17 -0500

    calculator files updated

--- lecture1-c/calculator.c
+++ lecture1-c/calculator.c
@@ -3,8 +3,9 @@
 
 int main(void)
 {
-    int x = get_int("x: ");
-    int y = get_int("y: ");
+    long x = get_int("x: ");
+    long y = get_int("y: ");
 
-    printf("%i\n", x + y);
+    double z = (double) x / (double) y;
+    printf("%.20f\n", z);
 }
