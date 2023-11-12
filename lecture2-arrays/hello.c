dddd1fbd3a55bed2b017b999044f95318f71f97a
Joseph Bak <joseph.bak31415@gmail.com>
2023-11-12 14:05:52 -0500

    compiling and linking

---
+++ lecture2-arrays/hello.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+#include <stdio.h>
+
+// To compile, clang -o hello hello.c -lcs50
+// -l meaning linking
+
+int main(void)
+{
+    string name = get_string("What's your name? ");
+    printf("hello, %s\n", name);
+}
