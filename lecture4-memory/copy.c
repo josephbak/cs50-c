3c37c18426db34a9291dd1291db4682e52d6abce
Joseph Bak <joseph.bak31415@gmail.com>
2023-11-14 20:01:28 -0500

    memory and garbage values in c

---
+++ lecture4-memory/copy.c
@@ -0,0 +1,43 @@
+// malloc and free
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(void)
+{
+    char *s = get_string("s: ");
+    if (s == NULL)
+    {
+        return 1;
+    }
+
+    char *t = malloc(strlen(s) + 1);
+    if (t == NULL)
+    {
+        return 1;
+    }
+
+    // int n;
+    // for (int i = 0; n = strlen(s) + 1; i < n; i++)
+    // {
+    //     t[i] = s[i];
+    // }
+
+    strcpy(t, s);
+
+    if (strlen(t) > 0)
+    {
+        t[0] = toupper(t[0]);
+    }
+
+    printf("s: %s\n", s);
+    printf("t: %s\n", t);
+
+    free(t);
+
+    return 0;
+
+}
