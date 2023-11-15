f8a7226ef689c9380d6fb5c603118bbb0084f234
Joseph Bak <joseph.bak31415@gmail.com>
2023-11-15 10:24:23 -0500

    getting user input in c

---
+++ lecture4-memory/get.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // int x;
+    // printf("x: ");
+    // scanf("%i", &x);
+    // printf("x: %i\n", x);
+
+    char s[4];
+    printf("s: ");
+    // strings are already address
+    scanf("%s", s);
+    printf("s: %s\n", s);
+}
